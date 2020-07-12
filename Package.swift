607ef61c74934fe01cee5f2f199660c3d607a4cd
Andrea <andrea.alessandro@gmail.com>
2020-07-12 19:53:27 +0200

    Fixed error with subscribe on

--- Package.swift
+++ Package.swift
@@ -17,7 +17,9 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+         .package(
+            url:"https://github.com/NordicSemiconductor/IOS-CoreBluetooth-Mock.git",
+            from: "0.8.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
