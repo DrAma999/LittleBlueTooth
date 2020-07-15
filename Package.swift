881310d60c96c125d21403018e92e405caf402bd
Andrea <andrea.alessandro@gmail.com>
2020-07-15 18:15:44 +0200

    Fixed swift package error

--- Package.swift
+++ Package.swift
@@ -22,8 +22,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
          .package(name: "CoreBluetoothMock",
                   url: "https://github.com/enricodk/IOS-CoreBluetooth-Mock.git",
-                  .branch("multiplatform"),
-                  from: "0.8.0"),
+                  .branch("multiplatform")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
