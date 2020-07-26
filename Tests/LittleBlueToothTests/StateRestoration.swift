dafc669b129932647a10889d032f5e76d97b503f
Andrea <andrea.alessandro@gmail.com>
2020-07-26 19:00:36 +0200

    Removed StateRestoration test

--- Tests/LittleBlueToothTests/StateRestoration.swift
+++ Tests/LittleBlueToothTests/StateRestoration.swift
@@ -10,13 +10,15 @@ import Combine
 import CoreBluetoothMock
 @testable import LittleBlueTooth
 
+
 class StateRestoration: LittleBlueToothTests {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
         try super.setUpWithError()
+        let peri = FakePeriph()
         CBMCentralManagerFactory.simulateStateRestoration = { (identifier) -> [String : Any]  in
-            return [ CBCentralManagerRestoredStatePeripheralsKey : [blinky] ]
+            return [ CBCentralManagerRestoredStatePeripheralsKey : [peri] ]
         }
       
        
@@ -27,14 +29,18 @@ class StateRestoration: LittleBlueToothTests {
     }
     
     func testStateRestore() {
-        let wrongCharacteristicExpectation = expectation(description: "State restoration")
+        let restoreExpectation = expectation(description: "State restoration")
 
         var littleBTConf = LittleBluetoothConfiguration()
-        littleBTConf.restoreHandler = { restored in
-            print("restored \(restored)")
-        }
         littleBTConf.centralManagerOptions = [CBMCentralManagerOptionRestoreIdentifierKey : "myIdentifier"]
         littleBT = LittleBlueTooth(with: littleBTConf)
+        
+        littleBT.restoreStatePublisher
+        .sink { (restorer) in
+            print(restorer)
+            restoreExpectation.fulfill()
+        }
+        
         
         waitForExpectations(timeout: 10)
     }
