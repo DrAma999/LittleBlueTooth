e822b051ea682a0b3a171590e7871af77001c869
Andrea <andrea.alessandro@gmail.com>
2021-09-18 22:35:20 +0200

    Fix issue with unknown CBCharecteristic service property attribute

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.4
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -25,7 +25,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         .package(name: "CoreBluetoothMock",
                  url: "https://github.com/NordicSemiconductor/IOS-CoreBluetooth-Mock.git",
-                 .upToNextMinor(from: "0.12.0")),
+                 .upToNextMinor(from: "0.13.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
